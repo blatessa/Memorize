d714170e2af51954037efba2d3d3bff299d9c65b
Brady LaTessa <brady@latessa.net>
2025-06-07 14:32:19 -0400

    Changed how cards are flipped and sized on the screen

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,17 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["💀", "👻", "🙈", "👋"]
+    let emojis = ["💀", "👻", "🙈", "👋", "👨‍👩‍👧‍👦", "🐶", "🐱", "🐭"]
+    
+    @State var cardCount = 2
     
     var body: some View {
-                
-        HStack {
-            ForEach(emojis.indices, id: \.self) { idx in
-                CardView(emojis[idx])
+        VStack {
+            ScrollView {
+                cards
             }
+            Spacer()
+            cardCountAdjusters
         }
         .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { idx in
+                CardView(emojis[idx])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
         .foregroundStyle(.orange)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardAdder
+            Spacer()
+            cardRemover
+        }
+        
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        }).disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+        
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
     }
 }
 
@@ -26,22 +64,22 @@ struct CardView: View {
     let content: String
     @State var isFaceUp: Bool
     
-    init(_ name: String, isFaceUp: Bool = false) {
+    init(_ name: String, isFaceUp: Bool = true) {
         self.content = name
         self.isFaceUp = isFaceUp
     }
     
     var body: some View {
         ZStack {
-            if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .fill(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 4)
-                Text("\(content)").font(.largeTitle)
-            } else {
-                RoundedRectangle(cornerRadius: 12)
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 4)
+                Text(content).font(.largeTitle)
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+            
         }.onTapGesture {
             isFaceUp.toggle()
         }
